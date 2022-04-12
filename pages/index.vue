9cdc14f45e69bee901419d108c22ee780d39307f
Cesar <cesargb@gmail.com>
2022-04-12 21:41:02 +0200

    fix menu

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,10 @@
         <a hreh="/news/laravel">Laravel</a>
       </li>
       <li>
-        <a href="/news/laravel">PHP</a>
+        <a href="/news/php">PHP</a>
       </li>
       <li>
-        <a href="/news/laravel">Nuxt</a>
+        <a href="/news/nuxt">Nuxt</a>
       </li>
     </ul>
   </div>
