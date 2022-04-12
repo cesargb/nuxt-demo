03d2f2590ab92a3402d0e8cb8b0ea55ac89b5e72
Cesar <cesargb@gmail.com>
2022-04-12 21:19:46 +0200

    tmenu ini

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1>Noticias</h1>
+    <ul>
+      <li>
+        <nuxt-link to="/news/laravel">Laravel</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/news/laravel">PHP</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/news/laravel">Nuxt</nuxt-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
