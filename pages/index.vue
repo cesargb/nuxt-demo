5cfb38fb44c1601680eff8a3a3a5b1ae8f4d6c92
Cesar <cesargb@gmail.com>
2022-04-12 21:37:32 +0200

    nuxt-link to a

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,13 @@
     <h1>Noticias</h1>
     <ul>
       <li>
-        <nuxt-link to="/news/laravel">Laravel</nuxt-link>
+        <a hreh="/news/laravel">Laravel</a>
       </li>
       <li>
-        <nuxt-link to="/news/laravel">PHP</nuxt-link>
+        <a href="/news/laravel">PHP</a>
       </li>
       <li>
-        <nuxt-link to="/news/laravel">Nuxt</nuxt-link>
+        <a href="/news/laravel">Nuxt</a>
       </li>
     </ul>
   </div>
